49a37302f262634e5c56027fdab88ea4f9ebb154
Marylucy123450 <marylucyatienoomenda@gmail.com>
2023-03-23 22:20:35 +0300

    created react app

---
+++ src/components/App.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import NavBar from "./NavBar";
+import Login from "../pages/Login";
+import TodoList from "../pages/TodoList";
+import NewTodo from "../pages/NewTodo";
+
+function App() {
+  const [user, setUser] = useState(null);
+
+  // useEffect(() => {
+  //   // auto-login
+  //   fetch("/me").then((r) => {
+  //     if (r.ok) {
+  //       r.json().then((user) => setUser(user));
+  //     }
+  //   });
+  // }, []);
+
+  // if (!user) return <Login onLogin={setUser} />;
+
+  return (
+    <>
+      <NavBar user={user} setUser={setUser} />
+      <main> 
+        <Routes>
+          <Route path="/new"  element={<NewTodo user={user} />}/>
+          <Route path="/todo" element={<TodoList />}/>
+          <Route path="/" element = {<Login onLogin={setUser} />}/>
+        </Routes>
+      </main>
+    </>
+  );
+}
+
+export default App;
